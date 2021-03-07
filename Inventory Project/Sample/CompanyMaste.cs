53f4a7a39debb3bfd76570841f54ed0297f1f665
idealtechinfo-8 <rajputpallavi.n@gmail.com>
2021-03-07 15:13:49 +0530

    Pallavi

--- Inventory Project/Sample/CompanyMaste.cs
+++ Inventory Project/Sample/CompanyMaste.cs
@@ -640,7 +640,11 @@ namespace sample
             this.WindowState = FormWindowState.Minimized;
         }
 
-       
+        private void txtBankName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["AdditinalFeild1"] = txtBankName.Text;
+            Properties.Settings.Default.Save();
+        }
     }
 
 }
