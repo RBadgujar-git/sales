2029a8ec900a2cfdff8d2c43648c8bf08a9b7cc4
djstar2721999@gmail.com <djstar2721999@gmail.com>
2021-03-11 20:03:13 +0530

    r

---
+++ Inventory Project/Sample/CompanyBankHomepage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sample
+{
+    public partial class CompanyBankHomepage : UserControl
+    {
+        public CompanyBankHomepage()
+        {
+            InitializeComponent();
+        }
+
+        private void CompanyBankHomepage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaleorder_Click(object sender, EventArgs e)
+        {
+            CompanyBankAccount cb = new CompanyBankAccount();
+            this.Controls.Add(cb);
+            cb.Dock = DockStyle.Fill;
+            cb.Visible = true;
+            cb.BringToFront();
+        }
+    }
+}
