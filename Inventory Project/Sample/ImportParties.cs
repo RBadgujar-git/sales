5e76dbc42371f88538532a746d8b4145dafd59b0
Rahul Badgujar <rbadgujar272@gmail.com>
2021-03-15 13:53:58 +0530

    rb

--- Inventory Project/Sample/ImportParties.cs
+++ Inventory Project/Sample/ImportParties.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace sample
 {
     public partial class ImportParties : UserControl
     {
-        string result = "D:\\Git project\\sales\\Inventory Project\\Sample\\bin\\Debug\\Import Parties Template.xls";
+        string result = "D:\\Git project\\sales\\Inventory Project\\Sample\\bin\\Debug\\ImportPartiesTemplate.xlsx";
+        SqlConnection con = new SqlConnection(Properties.Settings.Default.InventoryMgntConnectionString);
 
         public ImportParties()
         {
@@ -34,8 +35,8 @@ namespace sample
         private void btnDownload_Click(object sender, EventArgs e)
         {
             SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "*.xls; | *.xlsx;";
-            sfd.FileName = "Import Party Template";
+            sfd.Filter = "*.xlsx | *.xls";
+            sfd.FileName = "ImportPartiesTemplate";
             sfd.Title = "Save Excel File";
             if (sfd.ShowDialog() == DialogResult.OK)
             {
@@ -66,7 +67,7 @@ namespace sample
                         System.Data.DataSet DtSet;
                         System.Data.OleDb.OleDbDataAdapter MyCommand;
                         MyConnection = new System.Data.OleDb.OleDbConnection(@"provider=Microsoft.Jet.OLEDB.4.0;;Data Source=" + strExcelPathName + ";Extended Properties=Excel 8.0;");
-                        MyCommand = new System.Data.OleDb.OleDbDataAdapter("select * from [Template$]", MyConnection);
+                        MyCommand = new System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", MyConnection);
                         MyCommand.TableMappings.Add("Table", "Net-informations.com");
                         DtSet = new System.Data.DataSet();
                         MyCommand.Fill(DtSet);
@@ -88,8 +89,56 @@ namespace sample
             }
             finally
             {
-                
+
             }
         }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // int i = 0;
+
+
+
+
+                int counter =Convert.ToInt32 (dataGridView1.RowCount.ToString())-1;
+
+               // MessageBox.Show("dta va"+counter);
+
+                for (int i = 0; i <counter; i++)
+
+                {
+                    SqlCommand cmd = new SqlCommand(" insert into tbl_PartyMaster (PartyName,ContactNo,BillingAddress,EmailID,GSTType,State,OpeningBal,AsOfDate,AddRemainder,PartyType,ShippingAddress,PartyGroup,PaidStatus) values(@PartyName,@ContactNo,@BillingAddress,@EmailID,@GSTType,@State,@OpeningBal,@AsOfDate,@AddRemainder,@PartyType,@ShippingAddress,@PartyGroup,@paidstatus)", con);
+                    cmd.Parameters.AddWithValue("@PartyName", dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    cmd.Parameters.AddWithValue("@ContactNo", dataGridView1.Rows[i].Cells[1].Value.ToString());
+                    cmd.Parameters.AddWithValue("@BillingAddress", dataGridView1.Rows[i].Cells[2].Value.ToString());
+                    cmd.Parameters.AddWithValue("@EmailID", dataGridView1.Rows[i].Cells[3].Value.ToString());
+                    cmd.Parameters.AddWithValue("@GSTType", dataGridView1.Rows[i].Cells[4].Value.ToString());
+                    cmd.Parameters.AddWithValue("@State", dataGridView1.Rows[i].Cells[5].Value.ToString());
+                    cmd.Parameters.AddWithValue("@OpeningBal", dataGridView1.Rows[i].Cells[6].Value.ToString());
+                    cmd.Parameters.AddWithValue("@AsOfDate", dataGridView1.Rows[i].Cells[7].Value.ToString());
+                    cmd.Parameters.AddWithValue("@AddRemainder", dataGridView1.Rows[i].Cells[8].Value.ToString());
+                    cmd.Parameters.AddWithValue("@PartyType", dataGridView1.Rows[i].Cells[9].Value.ToString());
+                    cmd.Parameters.AddWithValue("@ShippingAddress", dataGridView1.Rows[i].Cells[10].Value.ToString());
+                    cmd.Parameters.AddWithValue("@PartyGroup", dataGridView1.Rows[i].Cells[11].Value.ToString());
+                    cmd.Parameters.AddWithValue("@paidstatus", dataGridView1.Rows[i].Cells[12].Value.ToString());
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Insert data Successfully");
+                    con.Close();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+                //throw;
+            }
+            finally
+            { con.Close(); }
+        }
     }
+
 }
