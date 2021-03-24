78c0462562f40233b8d9e7db5eb7035399721f76
Idealtechinfo-7 <vithobamali1998@gmail.com>
2021-03-24 16:44:29 +0530

    hhhhhh

--- Inventory Project/Sample/AdjustAccount.Designer.cs
+++ Inventory Project/Sample/AdjustAccount.Designer.cs
@@ -239,7 +239,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(331, 16);
+            this.label1.Location = new System.Drawing.Point(327, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(158, 18);
             this.label1.TabIndex = 138;
